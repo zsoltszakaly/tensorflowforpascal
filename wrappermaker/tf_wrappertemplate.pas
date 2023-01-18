7f1604b57745cfb965b923e679d14a10343e84d9
zsoltszakaly <61019600+zsoltszakaly@users.noreply.github.com>
2023-01-18 14:10:25 +0100

    Version for Tensorflow 2.11

--- wrappermaker/tf_wrappertemplate.pas
+++ wrappermaker/tf_wrappertemplate.pas
@@ -4,7 +4,7 @@ unit tf_wrapper;
 //
 //  Pascal methods to create and manage TensorFlow based Operations in a Specific manner
 //
-//  Copyright: (C) 2020, Zsolt Szakaly
+//  Copyright: (C) 2020-2023, Zsolt Szakaly
 //
 //  This source is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
 //  published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
